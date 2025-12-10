617ee00889933c21aa8483ede41b67c932e2b366
NEW BROTHERS STUDIOS <nohemymelgar027@gmail.com>
2025-12-10 16:09:48 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const videos = [
   {
-    titulo: "METAMORPHOSIS [NEW]",
+    titulo: "METAMORPHOSIS (♥ 76M)---
+  (👎 1.9M)",
     archivo: "https://newbrothersstudios.github.io/GetBrainrotsFree/videos/METAMORPHOSIS(360P).mp4"
   },
   {
-    titulo: "LOXKS edit [kathyzx]",
+    titulo: "LOXKS edit [kathyzx] (❤️ 62)----(👎 9) ",
     archivo: "https://newbrothersstudios.github.io/GetBrainrotsFree/videos/VID-20251210-WA0000.mp4"
   },
   {
-    titulo: "Cats edit [NEW]",
+    titulo: "Ya es navidad! (❤️ 102)----(👎 5)",
     archivo: "https://newbrothersstudios.github.io/GetBrainrotsFree/videos/48f6c404ffb4b41be65fe4112501f0af.mp4"
   }
   ];
